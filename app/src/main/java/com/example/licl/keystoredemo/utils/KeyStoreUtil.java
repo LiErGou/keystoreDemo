25e130b6ef8ab361bb8b4485aa20b4ed3bf7ecea
LiErGou <lichangliangsky@126.com>
2018-07-19 12:48:37 +0800

    第二版demo

--- app/src/main/java/com/example/licl/keystoredemo/utils/KeyStoreUtil.java
+++ app/src/main/java/com/example/licl/keystoredemo/utils/KeyStoreUtil.java
@@ -8,6 +8,9 @@ import android.util.Base64;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -94,13 +97,15 @@ public class KeyStoreUtil {
 
             Cipher inCipher=Cipher.getInstance("RSA/ECB/PKCS1Padding", "AndroidOpenSSL");
             inCipher.init(Cipher.ENCRYPT_MODE,publicKey);
+
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             CipherOutputStream cipherOutputStream = new CipherOutputStream(
                     outputStream, inCipher);
             cipherOutputStream.write(initialText.getBytes("UTF-8"));
             cipherOutputStream.close();
             byte [] vals = outputStream.toByteArray();
-            return Base64.encodeToString(vals, Base64.DEFAULT);
+            String finished=Base64.encodeToString(vals, Base64.DEFAULT);
+            return finished;
         }catch (Exception e){
             e.printStackTrace();
             Log.e(TAG, Log.getStackTraceString(e));
@@ -109,6 +114,7 @@ public class KeyStoreUtil {
     }
 
     public String decryptString(String cipherText){
+
         try{
             if (mAlias==null||mAlias.isEmpty()){
                 return "alias is null or empty";
@@ -140,6 +146,118 @@ public class KeyStoreUtil {
             Toast.makeText(mContext,"Exception " + e.getMessage() + " occured", Toast.LENGTH_LONG).show();
             Log.e(TAG, Log.getStackTraceString(e));
             return "Decrypt Failed";
+        }
+    }
+
+    public boolean encryptFile(String srcFile,
+                              String destionFile){
+        int len = 0;
+        byte[] buffer = new byte[128];
+        byte[] cipherbuffer = null;
+        try{
+            if (mAlias==null||mAlias.isEmpty()){
+                return false;
+            }
+            if(!mKeyStore.containsAlias(mAlias)){
+                return false;
+            }
+            KeyStore.PrivateKeyEntry privateKeyEntry=(KeyStore.PrivateKeyEntry
+                    ) mKeyStore.getEntry(mAlias,null);
+            RSAPublicKey publicKey=(RSAPublicKey)
+                    privateKeyEntry.getCertificate().getPublicKey();
+
+            Cipher inCipher=Cipher.getInstance("RSA/ECB/PKCS1Padding",
+                    "AndroidOpenSSL");
+            inCipher.init(Cipher.ENCRYPT_MODE,publicKey);
+
+            FileInputStream fis = new FileInputStream(new File(srcFile));
+            FileOutputStream fos = new FileOutputStream(new File(destionFile));
+
+            // 读取原文，加密并写密文到输出文件。
+            while ((len = fis.read(buffer)) != -1) {
+                cipherbuffer = inCipher.update(buffer, 0, len);
+                fos.write(cipherbuffer);
+                fos.flush();
+            }
+            cipherbuffer = inCipher.doFinal();
+            fos.write(cipherbuffer);
+            fos.flush();
+
+            if (fis != null)
+                fis.close();
+            if (fos != null)
+                fos.close();
+
+//            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//            CipherOutputStream cipherOutputStream = new CipherOutputStream(
+//                    outputStream, inCipher);
+//            cipherOutputStream.write(initialText.getBytes("UTF-8"));
+//            cipherOutputStream.close();
+//            byte [] vals = outputStream.toByteArray();
+//            String finished=Base64.encodeToString(vals, Base64.DEFAULT);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            Log.e(TAG, Log.getStackTraceString(e));
+            return false;
+        }
+    }
+
+    public boolean decryptFile(String srcFile,
+                              String destionFile){
+        int len = 0;
+        byte[] buffer = new byte[5 * 1024];
+        byte[] plainbuffer = null;
+
+        try{
+            if (mAlias==null||mAlias.isEmpty()){
+                return false;
+            }
+            if(!mKeyStore.containsAlias(mAlias)){
+                return false;
+            }
+            KeyStore.PrivateKeyEntry privateKeyEntry=(KeyStore.PrivateKeyEntry) mKeyStore.getEntry(mAlias,null);
+
+            Cipher output=Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            output.init(Cipher.DECRYPT_MODE,privateKeyEntry.getPrivateKey());
+
+
+            FileInputStream fis = new FileInputStream(new File(srcFile));
+            FileOutputStream fos = new FileOutputStream(new File(destionFile));
+
+            while ((len = fis.read(buffer)) != -1) {
+                plainbuffer = output.update(buffer, 0, len);
+                fos.write(plainbuffer);
+                fos.flush();
+            }
+
+            plainbuffer = output.doFinal();
+            fos.write(plainbuffer);
+            fos.flush();
+
+            if (fis != null)
+                fis.close();
+            if (fos != null)
+                fos.close();
+//            CipherInputStream cipherInputStream=new CipherInputStream(new ByteArrayInputStream(Base64.decode(cipherText,
+//                    Base64.DEFAULT)), output);
+//            ArrayList<Byte> values=new ArrayList<>();
+//            int nextByte;
+//            while ((nextByte = cipherInputStream.read()) != -1) {
+//                values.add((byte)nextByte);
+//            }
+//
+//            byte[] bytes = new byte[values.size()];
+//            for(int i = 0; i < bytes.length; i++) {
+//                bytes[i] = values.get(i).byteValue();
+//            }
+//
+//            String finalText = new String(bytes, 0, bytes.length, "UTF-8");
+            return true;
+        }catch (Exception e){
+            Toast.makeText(mContext,"Exception " + e.getMessage() + " occured", Toast.LENGTH_LONG).show();
+            Log.e(TAG, Log.getStackTraceString(e));
+            return false;
         }
     }
 
