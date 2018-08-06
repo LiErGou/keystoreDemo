d50ea1e2e523196ad977050234582fdf14810866
LiErGou <lichangliangsky@126.com>
2018-08-06 13:43:19 +0800

    初步实现文件加密解密

--- app/src/main/java/com/example/licl/keystoredemo/utils/KeyStoreUtil.java
+++ app/src/main/java/com/example/licl/keystoredemo/utils/KeyStoreUtil.java
@@ -148,8 +148,7 @@ public class KeyStoreUtil {
             return "Decrypt Failed";
         }
     }
-
-    public boolean encryptFile(String srcFile,
+        public boolean encryptFile(String srcFile,
                               String destionFile){
         int len = 0;
         byte[] buffer = new byte[128];
@@ -171,6 +170,9 @@ public class KeyStoreUtil {
             inCipher.init(Cipher.ENCRYPT_MODE,publicKey);
 
             FileInputStream fis = new FileInputStream(new File(srcFile));
+
+
+
             FileOutputStream fos = new FileOutputStream(new File(destionFile));
 
             // 读取原文，加密并写密文到输出文件。
@@ -200,7 +202,7 @@ public class KeyStoreUtil {
     public boolean decryptFile(String srcFile,
                               String destionFile){
         int len = 0;
-        byte[] buffer = new byte[5 * 1024];
+        byte[] buffer = new byte[256];
         byte[] plainbuffer = null;
 
         try{
@@ -217,36 +219,14 @@ public class KeyStoreUtil {
 
 
             FileInputStream fis = new FileInputStream(new File(srcFile));
+            CipherInputStream in = new CipherInputStream(fis, output);
             FileOutputStream fos = new FileOutputStream(new File(destionFile));
 
-            while ((len = fis.read(buffer)) != -1) {
-                plainbuffer = output.update(buffer, 0, len);
-                fos.write(plainbuffer);
-                fos.flush();
+            byte[] b = new byte[1024];
+            int numberOfBytedRead;
+            while ((numberOfBytedRead = in.read(b)) >= 0) {
+                fos.write(b, 0, numberOfBytedRead);
             }
-
-            plainbuffer = output.doFinal();
-            fos.write(plainbuffer);
-            fos.flush();
-
-            if (fis != null)
-                fis.close();
-            if (fos != null)
-                fos.close();
-//            CipherInputStream cipherInputStream=new CipherInputStream(new ByteArrayInputStream(Base64.decode(cipherText,
-//                    Base64.DEFAULT)), output);
-//            ArrayList<Byte> values=new ArrayList<>();
-//            int nextByte;
-//            while ((nextByte = cipherInputStream.read()) != -1) {
-//                values.add((byte)nextByte);
-//            }
-//
-//            byte[] bytes = new byte[values.size()];
-//            for(int i = 0; i < bytes.length; i++) {
-//                bytes[i] = values.get(i).byteValue();
-//            }
-//
-//            String finalText = new String(bytes, 0, bytes.length, "UTF-8");
             return true;
         }catch (Exception e){
             Toast.makeText(mContext,"Exception " + e.getMessage() + " occured", Toast.LENGTH_LONG).show();
@@ -254,6 +234,115 @@ public class KeyStoreUtil {
             return false;
         }
     }
+
+
+//    public boolean encryptFile(String srcFile,
+//                              String destionFile){
+//        int len = 0;
+//        byte[] buffer = new byte[128];
+//        byte[] cipherbuffer = null;
+//        try{
+//            if (mAlias==null||mAlias.isEmpty()){
+//                return false;
+//            }
+//            if(!mKeyStore.containsAlias(mAlias)){
+//                return false;
+//            }
+//            KeyStore.PrivateKeyEntry privateKeyEntry=(KeyStore.PrivateKeyEntry
+//                    ) mKeyStore.getEntry(mAlias,null);
+//            RSAPublicKey publicKey=(RSAPublicKey)
+//                    privateKeyEntry.getCertificate().getPublicKey();
+//
+//            Cipher inCipher=Cipher.getInstance("RSA/ECB/PKCS1Padding",
+//                    "AndroidOpenSSL");
+//            inCipher.init(Cipher.ENCRYPT_MODE,publicKey);
+//
+//            FileInputStream fis = new FileInputStream(new File(srcFile));
+//            FileOutputStream fos = new FileOutputStream(new File(destionFile));
+//
+//            // 读取原文，加密并写密文到输出文件。
+//            while ((len = fis.read(buffer)) != -1) {
+//                cipherbuffer = inCipher.update(buffer, 0, len);
+//                fos.write(cipherbuffer);
+//                fos.flush();
+//            }
+//            cipherbuffer = inCipher.doFinal();
+//            fos.write(cipherbuffer);
+//            fos.flush();
+//
+//            if (fis != null)
+//                fis.close();
+//            if (fos != null)
+//                fos.close();
+//
+//
+//            return true;
+//        }catch (Exception e){
+//            e.printStackTrace();
+//            Log.e(TAG, Log.getStackTraceString(e));
+//            return false;
+//        }
+//    }
+//
+//    public boolean decryptFile(String srcFile,
+//                              String destionFile){
+//        int len = 0;
+//        byte[] buffer = new byte[256];
+//        byte[] plainbuffer = null;
+//
+//        try{
+//            if (mAlias==null||mAlias.isEmpty()){
+//                return false;
+//            }
+//            if(!mKeyStore.containsAlias(mAlias)){
+//                return false;
+//            }
+//            KeyStore.PrivateKeyEntry privateKeyEntry=(KeyStore.PrivateKeyEntry) mKeyStore.getEntry(mAlias,null);
+//
+//            Cipher output=Cipher.getInstance("RSA/ECB/PKCS1Padding");
+//            output.init(Cipher.DECRYPT_MODE,privateKeyEntry.getPrivateKey());
+//
+//
+//            FileInputStream fis = new FileInputStream(new File(srcFile));
+//            FileOutputStream fos = new FileOutputStream(new File(destionFile));
+//
+//            while ((len = fis.read(buffer)) != -1) {
+//                plainbuffer = output.update(buffer, 0, len);
+//                Log.w("KeyStoreUtil","buffer: "+new String(buffer));
+//                Log.w("KeyStoreUtil","plainbuffer: "+plainbuffer);
+//                fos.write(plainbuffer);
+//                fos.flush();
+//            }
+//
+//            plainbuffer = output.doFinal();
+//            fos.write(plainbuffer);
+//            fos.flush();
+//
+//            if (fis != null)
+//                fis.close();
+//            if (fos != null)
+//                fos.close();
+////            CipherInputStream cipherInputStream=new CipherInputStream(new ByteArrayInputStream(Base64.decode(cipherText,
+////                    Base64.DEFAULT)), output);
+////            ArrayList<Byte> values=new ArrayList<>();
+////            int nextByte;
+////            while ((nextByte = cipherInputStream.read()) != -1) {
+////                values.add((byte)nextByte);
+////            }
+////
+////            byte[] bytes = new byte[values.size()];
+////            for(int i = 0; i < bytes.length; i++) {
+////                bytes[i] = values.get(i).byteValue();
+////            }
+////
+////            String finalText = new String(bytes, 0, bytes.length, "UTF-8");
+//            return true;
+//        }catch (Exception e){
+//            Toast.makeText(mContext,"Exception " + e.getMessage() + " occured", Toast.LENGTH_LONG).show();
+//            Log.e(TAG, Log.getStackTraceString(e));
+//            return false;
+//        }
+//    }
 
     public void deleteKey(final String alias){
         try {
